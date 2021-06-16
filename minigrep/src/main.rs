8307a45a2d43ee06a3f93a689ad1d0c1047cbef8
Nicola Benaglia <nbenaglia@cuebiq.com>
2021-06-16 18:44:09 +0200

    Print error message

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,6 +6,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
