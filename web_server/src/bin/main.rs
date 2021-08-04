85f7ae8a3c5c4f58b780ba23c22565ecda62e406
Nicola Benaglia <nico.benaz@gmail.com>
2021-08-04 19:37:10 +0200

    Add termination

--- web_server/src/bin/main.rs
+++ web_server/src/bin/main.rs
@@ -1,12 +1,10 @@
+use web_server::ThreadPool;
 use std::fs;
-use std::thread;
 use std::io::prelude::*;
-use std::time::Duration;
 use std::net::TcpListener;
 use std::net::TcpStream;
-
-
-use web_server::ThreadPool;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -19,16 +17,18 @@ fn main() {
             handle_connection(stream);
         });
     }
+
+    println!("Shutting down.");
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
-
     let get = b"GET / HTTP/1.1\r\n";
     let sleep = b"GET /sleep HTTP/1.1\r\n";
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
     let (status_line, filename) = if buffer.starts_with(get) {
         ("HTTP/1.1 200 OK", "hello.html")
